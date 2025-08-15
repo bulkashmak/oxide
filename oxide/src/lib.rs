d21e4f991cf3938bbd9b4bb21a709b9b7172ddd1
Bulat <mbmhd2015@gmail.com>
2025-08-15 12:33:19 +0500

    Add a loop

--- oxide/src/lib.rs
+++ oxide/src/lib.rs
@@ -1,15 +1,32 @@
 pub mod log;
 
-pub struct Application;
+pub struct Application {
+    running: bool,
+}
 
 impl Application {
     pub fn new() -> Self {
         log::init();
         log::info!("Oxide Application created!");
-        Self
+        Self { running: true }
     }
 
-    pub fn run(&self) {
-        log::info!("Oxide Application running...");
+    pub fn run(&mut self) {
+        log::info!("Oxide Application starting main loop...");
+
+        while self.running {
+            self.update();
+        }
+    }
+
+    fn update(&self) {
+        log::debug!("Application update tick...");
+        // Simulate doing work
+        std::thread::sleep(std::time::Duration::from_millis(500));
+    }
+
+    pub fn stop(&mut self) {
+        log::info!("Stopping Oxide Application...");
+        self.running = false;
     }
 }
--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -3,6 +3,17 @@ use oxide::Application;
 fn main() {
     oxide::log::info!("Sandbox starting...");
 
-    let app = Application::new();
+    let mut app = Application::new();
+    
+    // Run in a separate thread for demonstration
+    std::thread::spawn(move || {
+        std::thread::sleep(std::time::Duration::from_secs(3));
+        // We'll stop after 3 seconds
+        // In future this will come from events
+        println!("Requesting shutdown...");
+        // We can't stop here yet without shared mutability; 
+        // will handle with proper events later
+    });
+
     app.run();
 }
